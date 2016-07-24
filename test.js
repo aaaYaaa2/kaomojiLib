e84cf6b10129d99cac49cdf997764c0e4b8af4c8
xc <xcicada824@gmail.com>
2016-07-24 13:43:11 -0400

    increase kaomoji number

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ var assert = require('assert');
 var fs = require('fs');
 // test for correct number of kaomoji
 describe('Kaomoji', function(){
-	it('should have 40 kaomojis', function(){
+	it('should have 71 kaomojis', function(){
 		var data = require('./kaomoji.json');
 		var keys = Object.keys(data);
 		var len = keys.length;
-		var kaoNum = 40;
+		var kaoNum = 71;
 		assert.equal(len,kaoNum);
 	});
 });
